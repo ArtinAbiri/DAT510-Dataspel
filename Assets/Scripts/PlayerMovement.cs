bfc9ef24e4c2d0caaf5f0bd45ee7ce71275b580d
= <=>
2022-02-15 01:56:23 +0100

    movement & graphics begun

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    private Animator anim;
+    private bool grounded;
+    
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        
+        //vänder gubben
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector2.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector2(-1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //Animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
