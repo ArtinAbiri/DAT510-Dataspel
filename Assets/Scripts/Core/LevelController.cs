ec9eee112506256b9f088d2132bfba10ae159252
= <=>
2022-03-01 00:14:00 +0100

    next level checkpoint

---
+++ Assets/Scripts/Core/LevelController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Core
+{
+    public class LevelController: MonoBehaviour
+    {
+        [SerializeField] private string nextScene;
+        [SerializeField] private Text nextSceneText;
+        private bool canInteract;
+        private void Start()
+        {
+            nextSceneText.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (canInteract && Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("go to scene: " + nextScene);
+                SceneManager.LoadScene(nextScene);
+            } 
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                nextSceneText.gameObject.SetActive(true);
+                canInteract = true;
+
+            }
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                nextSceneText.gameObject.SetActive(false);
+                canInteract = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Drunk/Drunkness.cs
+++ Assets/Scripts/Drunk/Drunkness.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Drunk
 {
@@ -10,7 +11,7 @@ namespace Drunk
         private void Awake()
         {
             currentDrunkness = startingDrunkness;
-           
+            InvokeRepeating("SoberUp", 1f, 30f);
         }
 
         public void Drink(float amount)
@@ -19,6 +20,25 @@ namespace Drunk
             currentDrunkness = Mathf.Clamp(currentDrunkness + amount, 0, 4);
         }
 
+        public void SoberUp()
+        {
+            SoberUp(0.5f);
+        }
+        public void SoberUp(float amount)
+        {   
+            
+            if (currentDrunkness <= 0)
+            {
+                Debug.Log("SoberUp damage taken");
+                gameObject.GetComponent<Health>().TakeDamage(1);
+            }
+            else
+            {
+                Debug.Log("SoberUp " + amount);
+                currentDrunkness = Mathf.Clamp(currentDrunkness - amount, 0, 4);    
+            }
+            
+        }
         private void Update()
         {
         }
