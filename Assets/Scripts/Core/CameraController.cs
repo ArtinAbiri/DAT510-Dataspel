3365d53946e8f0c38463192f5cdc1ee890261e07
= <=>
2022-03-01 06:16:44 +0100

    lvl 2 collision fixat

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    
+
+    [SerializeField] private bool allowYMovement;
+
+    [SerializeField] private float lowestY;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        if (allowYMovement)
+        {
+            transform.position = new Vector3(player.position.x, Mathf.Clamp(player.position.y, lowestY, 1000), transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);   
+        }
     }
 }
