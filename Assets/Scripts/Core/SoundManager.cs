298faafdb6b5a10f7d0c75a8d9194813453b8321
= <=>
2022-03-01 03:10:58 +0100

    trash main menu

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -6,15 +6,23 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance { get; private set;}
     private AudioSource source;
-
+    private float mvol; // Global music volume
     private void Awake()
     {
         instance = this;
         source = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+        mvol = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        source.volume = mvol;
     }
 
     public void PlaySound(AudioClip sound)
     {
         source.PlayOneShot(sound);
+    }
+    
+    public void musicVolumeChanged() {
+        mvol = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        source.volume = mvol;
     }
 }
